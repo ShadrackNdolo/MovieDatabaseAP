b05d2d95ef268dd7ffd808ed9ffe7c0856c1897f
ShadrackNdolo <shadrack.winters@outlook.com>
2017-11-27 14:58:57 +0300

    movie page function that returns the movie page details and data

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template
+from app import app
+
+
+
+@app.route('/movie/<movie_id>')
+def movie(movie_id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = movie_id)
