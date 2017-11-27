c584ffdfed1e186db7db69967ea9b313ff4fbd4a
ShadrackNdolo <shadrack.winters@outlook.com>
2017-11-27 17:51:50 +0300

    make the API call to get a particular category of movies

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-
+from .request import get_movies
 
 
 @app.route('/movie/<movie_id>')
@@ -11,11 +11,15 @@ def movie(movie_id):
     '''
     return render_template('movie.html',id = movie_id)
 
+@app.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
 
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    print(popular_movies)
     title = 'Home - Welcome to The best Movie Review Website Online'
-    return render_template('index.html', title = title)
+    return render_template('index.html', title = title,popular = popular_movies)
