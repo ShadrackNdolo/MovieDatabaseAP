553bc3224e47394b3456db584165632f814a7ba8
ShadrackNdolo <shadrack.winters@outlook.com>
2017-11-29 14:28:20 +0300

    pip freeze lists all python dependancies

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ class Config:
 
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{550}?api_key={b7fe728244a843091e8a18442c4fd6b7'
     MOVIE_API_KEY = os.environ.get('b7fe728244a843091e8a18442c4fd6b7')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.environ.get('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2ZlNzI4MjQ0YTg0MzA5MWU')
 
 
 class ProdConfig(Config):
---
+++ start.sh
@@ -0,0 +1,4 @@
+ export MOVIE_API_KEY='b7fe728244a843091e8a18442c4fd6b7'
+export SECRET_KEY='eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiN2ZlNzI4MjQ0YTg0MzA5MWU'
+
+python3.6 manage.py server
