4743c951d39aeb44446d75999edee84bb5d58f11
ShadrackNdolo <shadrack.winters@outlook.com>
2017-11-29 12:38:10 +0300

    Learning how to use bootstrap

--- app/views.py
+++ app/views.py
@@ -1,14 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_movies
-
-@app.route('/movie/<movie_id>')
-def movie(movie_id):
-
-    '''
-    View movie page function that returns the movie details page and its data
-    '''
-    return render_template('movie.html',id = movie_id)
+from .requests import get_movies,get_movie
 
 @app.route('/')
 def index():
@@ -22,3 +14,14 @@ def index():
     print(popular_movies)
     title = 'Home - Welcome to The best Movie Review Website Online'
     return render_template('index.html', title = title,popular = popular_movies)
+
+@app.route('/movie/<int:id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
